7d795cefd051445763e0b9453133575cb8e93a08
BernhardLo <bernhardlo@hotmail.com>
2016-09-20 11:37:17 +0200

    dropdown för språk

---
+++ src/Project_MsCognitiveApi/wwwroot/Scripts/Translate.js
@@ -0,0 +1,14 @@
+﻿function sendToTranslate(key, lang) {
+    $.ajax({
+        url: "https://www.googleapis.com/language/translate/v2?key=" + key + "&q=hello%20world&source=en&target=de",
+        processData: false,
+        type: 'POST',
+        data: makeblob(picture)
+    })
+.done(function (data) {
+    resolve(data);
+})
+.fail(function () {
+    reject('error');
+});
+};
